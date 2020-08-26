38fcd7bf35971865ccf169de3b6bf2cabc199889
Deepinder Singh <deepindersingh@Deepinders-MacBook-Air.local>
2020-08-26 07:55:17 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/mtech_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mtech_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  double num1 = 0.0, num2 = 0.0, result = 0.0;
+
+  final TextEditingController t1 = new TextEditingController();
+  final TextEditingController t2 = new TextEditingController();
+
+  void doAddition() {
+    setState(() {
+      num1 = double.parse(t1.text);
+      num2 = double.parse(t2.text);
+      result = num1 + num2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      num1 = double.parse(t1.text);
+      num2 = double.parse(t2.text);
+      result = num1 - num2;
+    });
+  }
+
+  void doMul() {
+    setState(() {
+      num1 = double.parse(t1.text);
+      num2 = double.parse(t2.text);
+      result = num1 * num2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      num1 = double.parse(t1.text);
+      num2 = double.parse(t2.text);
+      result = num1 / num2;
+    });
+  }
+
+  void doClear() {
+    setState(() {
+      num1 = 0;
+      num2 = 0;
+      t1.text = '0';
+      t2.text = '0';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: new AppBar(
+        title: Text('Calculator'),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'Output : ${result.toString()}',
+              style: new TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.purple,
+              ),
+            ),
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: 'Enter Number 1'),
+              controller: t1,
+            ),
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: 'Enter Number 2'),
+              controller: t2,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text('+'),
+                  onPressed: doAddition,
+                  color: Colors.greenAccent,
+                ),
+                new MaterialButton(
+                  child: new Text('-'),
+                  onPressed: doSub,
+                  color: Colors.greenAccent,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text('*'),
+                  onPressed: doMul,
+                  color: Colors.greenAccent,
+                ),
+                new MaterialButton(
+                  child: new Text('/'),
+                  onPressed: doDiv,
+                  color: Colors.greenAccent,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                new MaterialButton(
+                  child: new Text('Clear'),
+                  onPressed: doClear,
+                  color: Colors.greenAccent,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
